ff065b834fed277b3d354ca21fca58da9068b669
captnw <williamngoc93@hotmail.com>
2020-08-05 17:12:45 -0700

    csvReader fixed for real this time I swear

--- bot.py
+++ bot.py
@@ -20,21 +20,21 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
         elif begin_phrase(message, ["!hello"]):
-            await message.channel.send('Hello {0.author.mention}'.format(message))
+            await message.channel.send(f"Hello {message.author.mention}")
         elif begin_phrase(message, ["!8ball","!Eightball","!8b"]):
             await message.channel.send("Gamer")
         elif begin_phrase(message, ["!m", "!members"]):
-            #channel_members = ""    
+            channel_members = ""    
             for member in message.channel.members:
-                #channel_members += member.name + ", "
+                channel_members += member.name + ", "
                 await message.channel.send(f"{member.name}'s status is {member.status}")
                 await message.channel.send(f"{member.name}'s color is {member.color}")
                 await message.channel.send(f"{member.name}'s role is {member.roles}")
                 
-            #await message.channel.send(f"{channel_members} are in the channel")
+            await message.channel.send(f"{channel_members} are in the channel")
     
     async def on_message_delete(self, message):
-        await message.channel.send(f"{message.author.name} tried to delete this message: {message.content}")
+        await message.channel.send(f"{message.author.name} tried to hide this message: {message.content}")
 
         
 
--- csvfile_reader.py
+++ csvfile_reader.py
@@ -1,45 +1,83 @@
-from tempfile import NamedTemporaryFile
-import shutil
+import os.path
+from os import path
 import csv
 
 filename = "private_data.csv"
 fields = ["name", "status"]
-temp_file = NamedTemporaryFile('w', newline='', delete=False)
+
+clear_safety = True # You're going to have to manually set this to false before calling the clear() function
+big_struct = [] # This will store + update all of the user info while the bot is still active
 
 
-def write_header() -> None:
-    ''' Deletes the whole .csv file apparently '''
-    with open(filename, "w+", newline='') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fields)
+# Note to anyone reading this
+# Instead of updating the .csv constantly, we would just read from .csv 
+# and maintain a list of dictionaries which would be updated
+# then when the bot shut down ... just update the .csv
+
+
+def csv_bootup() -> None:
+    ''' Parses the current .csv and stores info into big_struct. This should only be called once at the beginning.
+        If the file doesn't exist, this will not create a new file. '''
+    if path.exists(filename):
+        with open(filename, "r") as csv_file_rd:
+            reader = csv.DictReader(csv_file_rd, fieldnames= fields)
+            for row in reader:
+                if row["name"] == "name" or row["status"] == "status": 
+                    # Do not copy the header into big_struct (avoid duplication)
+                    continue
+                big_struct.append({"name": row["name"], "status": row["status"]})
+   
+
+def csv_shutdown() -> None:
+    ''' Gets all of the info in big_struct and then override/append it into private_data.csv '''
+    # Note that opening the file in "w+" mode will wipe the .csv file clean
+    with open(filename, "w+", newline='') as csv_file_wr:
+        # Sort the list of dictionaries by name ...
+        global big_struct
+        big_struct = sorted(big_struct, key = lambda dicta : dicta["name"])
+
+        # Then write the new stuff in the .csv file
+        writer = csv.DictWriter(csv_file_wr, fieldnames= fields)
         writer.writeheader()
+        for dicta in big_struct:
+            #print(dicta) # DEBUG PRINT
+            writer.writerow(dicta)
 
 
-def write_into(lista : list) -> None:
-    ''' Inserts a new row (name, status) ... replaces an existing row if the name already exists'''
-    with open(filename, "r", newline='') as csv_file, temp_file:
-        # First check if the data exists in .csv file
-        reader = csv.DictReader(csv_file, fieldnames=fields)
-        temp_writer = csv.DictWriter(temp_file, fieldnames=fields)
-        edit = False
+def csv_write_into(lista : list) -> None:
+    ''' Modifies / adds a new entry to big_struct '''
+    name, status = lista[0],lista[1]
+    nameExist = False
 
-        for row in reader:
-            if row["name"] == lista[0]:
-                edit = True
-                row["name"], row["status"] = lista[0], lista[1]
-            row = {"name": row["name"], "status": row["status"]}
-            temp_writer.writerow(row)
+    for dicta in big_struct:
+        if dicta["name"] == name:
+            # Name already exists, so just modify the status and other information
+            dicta["status"] = status
+            nameExist = True
 
-        # <Purpose> writes data into the csv
-        if (not edit):
-            temp_writer.writerow({"name": lista[0], "status": lista[1]})
+    if not nameExist:
+        # Name doesn't exist, so we have to add it to our big_struct
+        big_struct.append({"name": name, "status": status})
 
 
-    shutil.move(temp_file.name, filename)
-        
-        
- # ONE AT A TIME       
+def csv_clear() -> bool:
+    ''' The big boy function that removes/clears the data in the .csv 
+        Global variable "clear_safety" must be set to False before
+        this function can clear the .csv. Returns whether the .csv
+        has been successfully cleared or not. '''
+    if clear_safety == False:
+        f = open(filename, 'r+')
+        f.truncate(0) # need '0' when using r+
+        f.seek(0,0) # return back to start of file
+    return not clear_safety
 
-#write_header()
-#write_into(['phat','online'])
-#write_into(['captnw', 'dnd'])
-#write_into(['phat', 'stupid'])
+
+# Commands should probably go along this order (as seen below)
+csv_bootup()
+csv_write_into(['phat','online'])
+csv_write_into(['captnw', 'dnd'])
+csv_write_into(['phat', 'stupid'])
+csv_write_into(["james's dog", "name is forgotten"])
+csv_shutdown()
+
+
