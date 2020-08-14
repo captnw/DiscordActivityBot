1c93c88b2a7f6ecdc8ba73fd4144bbee1521b903
James <haimenguyen@gmail.com>
2020-08-14 14:18:23 -0700

    Last debugs

--- graph_producer.py
+++ graph_producer.py
@@ -8,21 +8,19 @@ def int_to_str(int_list : list) -> list:
     return [str(item) if item != 0 else str(0) for item in int_list]
 
 def produce_graph(data: list, id: str) -> None:
+    plt.style.use('seaborn')
+    zero_list = [0 for zero in range(24)]
+    hour_x = [num for num in range(1, 25)]
+    plt.scatter(hour_x, int_to_str(zero_list))
+    plt.title("Active Hours on discord")
+    plt.xlabel("Hours Online")
+    plt.ylabel("Day")
+    plt.xticks(hour_x)
     for day in range(len(data)):
         day_legend = list(data[day].keys())[0] 
         bool_activity_list = list(data[day].values())[0]
         activity_list = activity_converter(bool_activity_list, int(day_legend))
         plt.scatter(hour_x, int_to_str(activity_list))
     plt.savefig(f"./graph_folder/{id}.png")
-    plt.show()
 
-plt.style.use('seaborn')
-zero_list = [0 for zero in range(24)]
-hour_x = [num for num in range(1, 25)]
-print(zero_list, hour_x)
-#plt.scatter(hour_x, int_to_str(zero_list))
 
-plt.title("Active Hours on discord")
-plt.xlabel("Hours Online")
-plt.ylabel("Day")
-plt.xticks(hour_x)
