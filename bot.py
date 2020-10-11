592ff0256df48876af2cccf3556ee80e03132c1c
captnw <williamngoc93@hotmail.com>
2020-10-10 20:01:42 -0700

    Finished sql replacing excel, commands are still broken

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-import secretTextfile, asyncio, datetime, pytz, sqlite3
+import secretTextfile, datetime, pytz
+from collections import defaultdict 
 from discord.ext.commands import Bot as BotBase
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-# from csvfile_reader import csv_bootup, csv_shutdown, csv_write_into, csv_lookup_schedule, csv_all_schedule, csv_order_data
 import sqlite_handler
-import os
+from os import system as osSYSTEM, name as osNAME
 from glob import glob
 
 # in this experimental branch, we're switching from csv to .db (using sqlite3)
@@ -14,55 +14,10 @@ from glob import glob
 COGS = [path.split("\\")[-1][:-3] for path in glob("./cogs/*.py")] # Fetch cog files
 VERSION = "0.5.0" # Bot version
 
-# OLD CSV stuff
-"""
-def check_online(client) -> None:
-    ''' Determines what members that the bot sees are online at the current hour and stores that info + their
-        status into a datastructure (a list of dicts which has a key of string and a value of list of ints)'''
-    pacific = pytz.timezone("US/Pacific") # The timezone being used to record the hour
-    now = datetime.datetime.now(pacific)
-    print(f"Checking who is online on {now.month}/{now.day:02}/{now.year} at {now.hour:02}:{now.minute:02}:{now.second:02} PST.")
-
-    online_people = ""
-
-    for member in sorted(client.get_all_members(), key = lambda x : str(x).split("#")[0].lower()):
-        if not (member.bot):
-            # Fetching all other data excluding schedule
-            # If added new labels, MUST ADD NEW KEY AND VALUE BELOW FOR AFOREMENTIONED LABELS
-            unordered_data = {"_NAME":str(member), "_NICKNAME":str(member.nick), "_ID":member.id, "_GUILD":str(hash(member.guild)), "_STATUS":str(member.status)}
-            old_schedule = csv_lookup_schedule(member.id, now.day)
-            if not (now.day in old_schedule[-1].keys()):
-                # Consider making a new dict if today is different
-                if len(old_schedule) == 10:
-                    # Pop the first element to make room for a new one
-                    # If we have 10 days worth of data already
-                    old_schedule.pop(0)
-                old_schedule.append({now.day : [0 for _ in range(24)]})
-
-            if str(member.status) != "offline":
-                # If the member is not offline, update the current hour
-                old_schedule[-1][now.day][now.hour-1] = old_schedule[-1][now.day][now.hour-1] | 1
-                online_people += member.name + ", "
-
-            unordered_data["_SCHEDULE"] = str(old_schedule)
-            member_data = csv_order_data(unordered_data)
-            csv_write_into(member_data)
-
-    csv_all_schedule() # update the online frequency list
-
-    if online_people != "":
-        online_people = online_people.rstrip(", ")
-        if (online_people.find(",")):
-            print("{} are online right now.\n".format(online_people))
-        else:
-            print("{} is online right now.\n".format(online_people))
-    else:
-        print("Nobody is online right now.\n")
-"""
 
 def header(*args) -> None:
     ''' Clears the terminal, print the header, and prints any additional strings passed in'''
-    os.system('cls' if os.name=='nt' else 'clear') # Clears the terminal
+    osSYSTEM('cls' if osNAME=='nt' else 'clear') # Clears the terminal
     print(f"\\\\_ScheduleBot v.{VERSION}_//")
     for string in args: print(string)
 
@@ -72,40 +27,48 @@ def check_update_online(client) -> None:
     status into a datastructure (a list of dicts which has a key of string and a value of list of ints)'''
     now = datetime.datetime.now(pytz.UTC) # UTC time
     header(f"Checking who is online on {now.month}/{now.day:02}/{now.year} at {now.hour:02}:{now.minute:02}:{now.second:02} UTC.")
-    online_people = ""
-
+    
+    online_server = defaultdict(list) # key - guild, val - list of people online
     all_member_data = [] # store all member data for a one time write open cycle (open sql, write in sql, close sql)
+    
     for member in sorted(client.get_all_members(), key = lambda x : str(x).split("#")[0].lower()):
         if (not (member.bot)):
+            unordered_data = {"NAME":str(member), "NICKNAME":str(member.nick), "ID":member.id, 
+                        "GUILD_HASH":str(hash(member.guild)), "STATUS":str(member.status)}
+            old_schedule = sqlite_handler.fetch_schedule(member.id, str(hash(member.guild)), now.day)
+
+            if not (now.day in old_schedule[-1].keys()):
+                # Consider making a new dict if today is different
+                if len(old_schedule) == 10:
+                    # Pop the first element to make room for a new one
+                    # If we have 10 days worth of data already
+                    old_schedule.pop(0)
+                old_schedule.append({now.day : [0 for _ in range(24)]})
             if str(member.status) != "offline":
-                online_people += member.name + ", "
-            dict_data = {"NAME":str(member), "NICKNAME":str(member.nick), "ID":member.id, 
-                        "GUILD_ID":str(hash(member.guild)), "STATUS":str(member.status)
-                        ,"SCHEDULE":str([[]])}
-            all_member_data.append(tuple(sqlite_handler.order_dict(dict_data)))
+                # If the member is not offline, update the current hour
+                old_schedule[-1][now.day][now.hour-1] = old_schedule[-1][now.day][now.hour-1] | 1
+                online_server[member.guild].append(member.name)
+            unordered_data["SCHEDULE"] = str(old_schedule)
+            all_member_data.append(tuple(sqlite_handler.order_dict(unordered_data)))
 
     sqlite_handler.insert_update(all_member_data)
-    
-    
-    
-    print("Insertion completed!")
 
-    if online_people != "":
-        online_people = online_people.rstrip(", ")
-        if (online_people.find(",")):
-            print("{} are online right now.\n".format(online_people))
-        else:
-            print("{} is online right now.\n".format(online_people))
-    else:
+    if not bool(online_server):
         print("Nobody is online right now.\n")
+    else:
+        for guild, member_list in online_server.items():
+            print(f"|| {str(guild)} server:")
+            online_people = (", ".join(member_list)).rstrip(", ")
+            if (online_people.find(",")):
+                print("{} are online right now.\n".format(online_people))
+            else:
+                print("{} is online right now.\n".format(online_people))
 
 
 class Bot(BotBase):
     def __init__(self):
         self.ready = False
         self.scheduler = AsyncIOScheduler()
-
-        #sqlite_handler.test_print(self.scheduler)
         super().__init__(command_prefix='!')
 
     def setup(self):
@@ -121,9 +84,6 @@ class Bot(BotBase):
         self.setup()
         self.TOKEN = secretTextfile.__TOKEN__
 
-        print("\nLoading .sqlite stuff")
-        #csv_bootup()
-
         print("\nRunning bot...")
         super().run(self.TOKEN, reconnect=True)
 
@@ -132,8 +92,6 @@ class Bot(BotBase):
 
     async def on_disconnect(self):
         header("Bot disconnected.")
-        print("Closing .sqlite file...")
-        #csv_shutdown()
 
     async def on_ready(self):
         now = datetime.datetime.now(pytz.UTC) # UTC time
@@ -158,5 +116,7 @@ if __name__ == "__main__":
     except Exception as e:
         print("\n___________ERROR_CAUGHT_______________")
         print("Error description is shown below...\n{}".format(e))
-        print("Closing .sqlite file...")
-        #csv_shutdown()
+        print("Shutting bot down...")
+        for job in bot.scheduler.get_jobs():
+            job.remove()
+        bot.scheduler.shutdown()
--- sqlite_handler.py
+++ sqlite_handler.py
@@ -1,20 +1,24 @@
-import sqlite3
+from ast import literal_eval as astEVAL
+from sqlite3 import connect as sqlite3CONNECT
+
+# This file handles the storing and moving of schedule data between the data.sqlite file and the bot
 
 sql_filename = "data"
 sql_filenameFULL = sql_filename+".sqlite"
-data_fields = [("NAME","TEXT DEFAULT none"), ("NICKNAME","TEXT DEFAULT none"), 
-    ("ID","INT PRIMARY KEY DEFAULT -1"), ("GUILD_ID","INT DEFAULT -1"), 
-    ("STATUS","TEXT DEFAULT none"), ("SCHEDULE","TEXT DEFAULT none")]
-#big_struct = [] # This will store + update all of the user info while the bot is still active
-#online_freq = {} # This will store the frequency/amount of people who are online at certain hours in a week.
+data_fields = [("NAME","TEXT DEFAULT None"), ("NICKNAME","TEXT DEFAULT None"), 
+    ("ID","INT PRIMARY KEY DEFAULT -1"), ("GUILD_HASH","INT DEFAULT -1"), 
+    ("STATUS","TEXT DEFAULT None"), ("SCHEDULE","TEXT DEFAULT None")]
 
-db = sqlite3.connect(sql_filenameFULL)
+online_freq = {} # This will store the frequency/amount of people who are online in any server at certain hours in a week.
+                 # Key: guild hash str, Value: a dict with keyval pair DAYS str:int and keyval pair FREQ str:[int (size 24)]
+## This code segment below runs when imported  ##
+
+db = sqlite3CONNECT(sql_filenameFULL)
 cursor = db.cursor()
-
 # Setup the table if it already doesn't exists.
 cursor.execute(f"CREATE TABLE IF NOT EXISTS {sql_filename}("
     + ", ".join(tup[0]+" "+tup[1] for tup in data_fields) + ")")
-print("Create sql file check done.")
+print("\nCreate sql file check done.")
 
 # Close cursor and db
 db.commit()
@@ -22,18 +26,47 @@ cursor.close()
 db.close()
 db, cursor = None, None
 
-# This will eventually replace the csvfile_reader.
+## Code segment ends ##
+
 
 def insert_update(lista: list) -> None:
+    ''' Updates the .sqlite file '''
     if not lista:
         # Empty, raise error.
         raise ValueError("Function sqlite_handler.insert_update(lista: list) should have a nonempty list as an argument")
-    db = sqlite3.connect(sql_filenameFULL)
+    db = sqlite3CONNECT(sql_filenameFULL)
     cursor = db.cursor()
     questions = (", ".join(["?" for _ in data_fields])).rstrip(", ") # generate the ?, ?, ?, ?
     cursor.executemany(f"INSERT OR REPLACE INTO {sql_filename} VALUES ({questions})", lista) # "upserting" = insert OR update, look it up.
+    cursor.close()
     db.commit()
     db.close()
+
+
+def fetch_schedule(member_id: int, guild_hash: str, current_day: int) -> list:
+    ''' Return someone's 24 hour activity (for n days, up to 10 days) which is denoted by a list of boolean values (true/false means
+        active for the hour at the index+1th hour). Otherwise returns a list of size 24 all set to false'''
+    db = sqlite3CONNECT(sql_filenameFULL)
+    cursor = db.cursor()
+    cursor.execute(f"SELECT SCHEDULE FROM {sql_filename} WHERE ID=?",(member_id,))
+    sched = cursor.fetchone()[0]
+    cursor.close()
+    db.close()
+
+    if not guild_hash in online_freq:
+        # Create the freq graph for the server if it didn't already exist
+        online_freq[guild_hash] = {"DAYS":1, "FREQ":[0 for _ in range(24)]}
+
+    if sched and sched != "none": 
+        sched = astEVAL(sched)
+
+        # Since we already have access to a member's schedule, traverse through schedule 
+        # and add the hours online to server freq graph
+        
+
+        return sched
+    else: 
+        return [{current_day:[0 for _ in range(24)]}]
 
 
 def order_dict(unordered_dict: dict) -> list:
